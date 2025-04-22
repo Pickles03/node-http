cc1259c32f11d4d6fa696e6b3f48f036faf2a7cb
Pickles03 <elizaveta.kris@gmail.com>
2025-04-22 12:48:48 +0200

    made the http server

---
+++ data.js
@@ -0,0 +1,10 @@
+//export from here some relevant info about a web page, for example a title, subtitle, description, keywords, and author
+const webPageData = {
+    title: 'My Web Page',
+    subtitle: 'Welcome to my web page',
+    description: 'This is a sample web page created to demonstrate the use of JavaScript.',
+    keywords: 'web, page, sample, JavaScript',
+};
+
+module.exports = {webPageData};
+
---
+++ server.js
@@ -0,0 +1,18 @@
+
+
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+    res.end(`
+        <h1>${webPageData.title}</h1>
+        <h2>${webPageData.subtitle}</h2>
+        <p>${webPageData.description}</p>
+        <p>${webPageData.keywords}</p>
+        `);
+});
+
+server.listen(0, () => {
+    console.log(`Server is listening on port: http://localhost:${server.address().port}`);
+})
+
+const {webPageData} = require('./data.js');
